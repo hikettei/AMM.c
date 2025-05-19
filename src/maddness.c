02900f7d0d291135d63bec356b500fb1828f3f55
hikettei <hikettei@gmail.com>
2025-05-19 16:40:23 +0900

    what to do next

--- src/maddness.c
+++ src/maddness.c
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-void fill_with_gaussian(float *X, int size) {
-  for (int i=0; i<size; i++) X[i] = sqrt(-2.0 * log((float)rand() / RAND_MAX)) * cos(2.0 * M_PI * (float)rand() / RAND_MAX);
+float* randn(int size) {
+  float *x = (float *)malloc(size * sizeof(float));
+  for (int i=0; i<size; i++) x[i] = sqrt(-2.0 * log((float)rand() / RAND_MAX)) * cos(2.0 * M_PI * (float)rand() / RAND_MAX);
+  return x;
 }
 
 // [TODO] 多分別のToplevelを作る
@@ -13,10 +15,21 @@ int main() {
   // Maddness Workflow
   // 1, Prototype Learning
   // Initialize matrix sampled from gaussian dist.
-  int M = 128, N = 128, LDX = 128;
-  float *X_Dummy = (float *)malloc(M * N * sizeof(float));
-  fill_with_gaussian(X_Dummy, M * N);
-  printf("X_Dummy: %f\n", X_Dummy[0]); // the seed is fixed right?
+  int N = 512, M = 512, K = 512;
+  int LDX = 512; // Column Major
+  int C = 16; // Number of Codebooks
+  int nsplits = 4; // Number of splits per codebook
+
+  // We are going to approximate A[N M] @ B[M K]
+  float *A_Offline = randn(M * N);
+  float *A         = randn(M * N);
+  // 1. SET_A_OFFLINE
+  
+  
+  // 2. SET_A
+  // 3. SET_B
+
+  
   // 2, Encoding Function
   // 3, Table Construction
   // 4, Aggregation
