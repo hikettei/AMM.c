efe0a84da7222a5d39b65e273ad919dc49c1ee7b
hikettei <hikettei@gmail.com>
2025-05-20 13:23:58 +0900

    start working on complete ndarray

--- include/ndarray.h
+++ include/ndarray.h
@@ -15,3 +15,10 @@ struct NDArray {
 
 NDArray* amm_ndarray_alloc(int ndim, int* shape, int* strides, void* storage, AMM_DType dtype);
 void amm_ndarray_free(NDArray* arr);
+// TODO: Naming Convention inspired from ISL
+// __amm_give__ __amm_take__ __amm_keep__
+#define __amm_give
+#define __amm_take
+#define __amm_keep
+
+__amm_give NDArray* amm_ndarray_give_randn();
